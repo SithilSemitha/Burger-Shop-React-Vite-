94b3225b97ea03add16a26ef3abdd367e720c1ba
SithilSemitha <sithilsemithad@gmail.com>
2026-01-07 08:14:55 +0530

    feat: Added the second set of Features

---
+++ src/assets/assest.js
@@ -0,0 +1,83 @@
+import background from './Background.png'
+import background2 from './Background2.png'
+import burgerLayer from './burger-layers.png.png'
+import burger1 from './burger12-430x430.jpg.png'
+import burger2 from './burger13-430x430.jpg.png'
+import burger3 from './burger16-430x430.jpg.png'
+import burger4 from './burger18-430x430.jpg.png'
+import burger5 from './burger20-430x430.jpg.png'
+import burger6 from './burger11-430x430.jpg.png'
+import beefBurger from './Figure → beef-chicken-600x386.png.png'
+import healthyBurger from './Figure → burger-healthy.png.png'
+import chickenBeef from './Figure → chicken-beef-600x386.png.png'
+import fries from './Figure → fries-healthy.png.png'
+import mightMeat from './Figure → mighty-meaty.png.png'
+import overlay from './Image.png'
+import logo from './istockphoto-1004591262-612x612 1.png'
+import logo2 from './Link.png'
+import backgroundLayer from './BackgroundLayer.png'
+
+
+export const foods = [
+  {
+    id: 1,
+    name: 'Western Bacon Angus Burger',
+    img: burger4,
+    description: 'Juicy Angus beef layered with crispy bacon, smoky BBQ sauce, cheddar cheese, fresh lettuce, and tomatoes on a toasted bun.'
+    , price: '$12.99'
+  },
+  {
+    id: 2,
+    name: 'Port Royal Eggsy Burger',
+    img: burger2,
+    description: 'A bold blend of beef and a sunny-side-up egg, crowned with grilled onions, cheese, and creamy aioli for a royal bite.'
+    , price: '$9.99'
+  },
+  {
+    id: 3,
+    name: 'Onion-y Angus Burger',
+    img: burger6,
+    description: 'Savory Angus patty stacked with golden onion rings, melty cheese, and zesty sauce, creating a burst of flavor in every bite.'
+    , price: '$11.88'
+  },
+  {
+    id: 4,
+    name: 'Portobello ‘Shroom Angus Burger',
+    img: burger5,
+    description: 'Succulent Angus beef meets grilled portobello mushrooms, Swiss cheese, garlic mayo, and fresh greens on a brioche bun.'
+    , price: '$14.99'
+  },
+  {
+    id: 5,
+    name: 'BBQ Bacon & Egg Burger',
+    img: burger1,
+    description: 'A smoky BBQ experience with crispy bacon, a fried egg, melted cheddar, and juicy beef for the ultimate breakfast-style burger.'
+    , price: '$13.88'
+  },
+  {
+    id: 6,
+    name: 'Western Bacon Cheeseburger',
+    img: burger3,
+    description: 'Classic beef burger topped with smoky bacon, melted cheese, crispy onion straws, and a slather of BBQ sauce.'
+    , price: '$14.99'
+  }
+]
+
+
+
+
+export {
+  background,
+  background2,
+  burgerLayer,
+  beefBurger,
+  healthyBurger,
+  chickenBeef,
+  fries,
+  mightMeat,
+  overlay,
+  logo,
+  logo2,
+  backgroundLayer,
+  burger1
+}
